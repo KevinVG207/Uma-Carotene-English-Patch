f8fe91ede8d09d3b4985ba66109312cfc065cf4d
KevinVG207 <kevin2000.van.geemen@gmail.com>
2024-03-01 16:28:33 +0100

    Fix api requests not going to umapyoi.net

--- src/util.py
+++ src/util.py
@@ -262,7 +262,7 @@ def fetch_latest_github_release(username, repo, prerelease=False):
     try:
         r = requests.get(url)
         r.raise_for_status()
-        if 200 <= r.status_code < 300:
+        if not (200 <= r.status_code < 300):
             raise Exception("Umapyoi.net API request failed")
         data = r.json()
     except:
