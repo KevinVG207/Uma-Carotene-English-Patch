e00564885766f9310e646f4b6dc49ec727156ec5
KevinVG207 <kevin2000.van.geemen@gmail.com>
2024-04-07 23:08:29 +0200

    Ensure corruption check happens every time.

--- src/unity.py
+++ src/unity.py
@@ -1,16 +1,32 @@
 import os
 import UnityPy
+import util
+import shutil
 
-def load_assetbundle(path):
+def load_assetbundle(path, hash):
     if not os.path.exists(path):
         raise FileNotFoundError(f"Path {path} does not exist. Cannot load assetbundle.")
     asset = UnityPy.load(path)
 
-    return list(asset.container.values())[0].get_obj()
+    try:
+        root = list(asset.container.values())[0].get_obj()
+    except IndexError:
+        root = None
+    
+    if root is None:
+        # Maybe the asset is corrupted.
+        # We try downloading it again.
+        util.download_asset(hash, no_progress=True)
+        shutil.copy(path, path + ".bak")
+        asset = UnityPy.load(path)
+        try:
+            root = list(asset.container.values())[0].get_obj()
+        except IndexError:
+            root = None
 
-def load_asset(path):
-    if not os.path.exists(path):
-        raise FileNotFoundError(f"Path {path} does not exist. Cannot load asset.")
-    asset = UnityPy.load(path)
+    return asset, root
 
-    return asset
+
+def load_asset_from_hash(hash):
+    path = util.get_asset_path(hash)
+    return load_assetbundle(path, hash)
