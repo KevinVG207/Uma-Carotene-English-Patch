f300d252783a373c34c40e79c1af8e974cb1b08c
KevinVG207 <kevin2000.van.geemen@gmail.com>
2024-05-10 21:52:01 +0200

    Undo max process limit

--- src/util.py
+++ src/util.py
@@ -55,8 +55,8 @@ class UmaPool(Pool):
         if not processes:
             processes = os.cpu_count() or 1
 
-        if processes > 12:
-            processes = 12
+        # if processes > 12:
+        #     processes = 12
 
         super().__init__(processes, *args, **kwargs)
 
@@ -155,6 +155,9 @@ class DMMConfigNotFoundException(Exception):
     pass
 
 def display_critical_message(title, text):
+    if is_script:
+        print(f"{title}: {text}")
+        return
     msg = QMessageBox()
     msg.setIcon(QMessageBox.Critical)
     msg.setText(text)
