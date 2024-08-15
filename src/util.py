a4b8616d4190695f6a402eeb94669be0772bb79d
KevinVG207 <kevin2000.van.geemen@gmail.com>
2024-08-15 16:46:39 +0200

    Code sync with TL repo.

--- src/util.py
+++ src/util.py
@@ -102,6 +102,9 @@ ASSEMBLY_FOLDER_EDITING = INTERMEDIATE_PREFIX + "assembly\\"
 
 DIFF_FOLDER = TL_PREFIX + "diff\\"
 
+GACHA_COMMENT_TL_PATH = ASSETS_FOLDER + "\\gacha\\comment\\translations.json"
+GACHA_COMMENT_TL_PATH_EDITING = ASSETS_FOLDER_EDITING + "\\gacha\\comment\\translations.json"
+
 TABLE_PREFIX = '_carotene'
 TABLE_BACKUP_PREFIX = TABLE_PREFIX + "_bak_"
 
